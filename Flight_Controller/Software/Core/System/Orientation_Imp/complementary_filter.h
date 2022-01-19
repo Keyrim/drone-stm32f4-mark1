7e177f1d0daca461a2ce26bf19814dffe2417777
Keyrim <40676844+Keyrim@users.noreply.github.com>
2022-01-19 17:50:43 +0100

    wip

---
+++ Flight_Controller/Software/Core/System/Orientation_Imp/complementary_filter.h
@@ -0,0 +1,14 @@
+/*
+ * complementary_filter.h
+ *
+ *  Created on: 19 janv. 2022
+ *      Author: Théo Magne
+ */
+
+#ifndef ORIENTATION_IMP_COMPLEMENTARY_FILTER_H_
+#define ORIENTATION_IMP_COMPLEMENTARY_FILTER_H_
+
+#include "../Orientation.h"
+
+
+#endif /* ORIENTATION_IMP_COMPLEMENTARY_FILTER_H_ */
