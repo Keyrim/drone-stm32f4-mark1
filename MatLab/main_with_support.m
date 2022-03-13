c8afb802eaf6a7df46fec2cefe50a04f25a9e3c0
Keyrim <40676844+Keyrim@users.noreply.github.com>
2022-03-13 14:51:22 +0100

    add configuration with support system

--- MatLab/main_with_support.m
+++ MatLab/main_with_support.m
@@ -1,28 +1,28 @@
 %% Paramètres physique du modèle
 % Matrice d'inertie du drone en g.mm^2
 format longg
-I = [1.862E+06 43.454	1.549E+05;
-	43.454 1.593E+06 23.054;
-	1.549E+05 23.054 2.606E+06];
+I = [2.320E+06 -62.113	-12.899;
+	-62.113 1.975E+06 3461.011;
+	-12.899 3461.011 3.242E+06];
 % Matrice d'inertie du drone kg.m^2
 I = I * 1E-9
 % Inverse de la matrice d'inertie du drone
 iInv = inv(I);
 % Coef force de frottement quand le drone tourne en kg*m*s^-2 / s * rad
-f = 0.8;
+f = 1;
 % Conversion signal moteur =>force en Newton(kg*m*s^-2)
-k = 0.01;
+k = 0.002;
 % Constance de couple en yaw
 C = 0.01;
 % Position du centre de gravité du drone en m
-centerOfMass = [0.049089 0.014532 0.027805];
+centerOfMass = [0.051621 -0.01692 0.108276];
 % Positions des moteurs en m
-motorsPositionAbsolute = [0.12689 0.10421 0.009599;
-    0.12689 -0.07516 0.009599;
-    -0.1058 -0.07516 0.009599;
-    -0.1058 0.10421 0.009599];
+motorsPositionAbsolute = [-0.038063 0.052022 0.094153;
+    0.141312 0.052022 0.094153;
+    0.141312 -0.085456 0.094153;
+   -0.038063 -0.085456 0.094153];
 % Positions des moteurs relativement au centre de gravité du drone
-mPos = motorsPositionAbsolute - centerOfMass;
+mPos = motorsPositionAbsolute - centerOfMass
 % Matrice pour obtenir les moments générer par les moteurs
 M = k*[mPos(1, 2) mPos(2, 2) mPos(3, 2) mPos(4, 2);
     -mPos(1, 1) -mPos(2, 1) -mPos(3, 1) -mPos(4, 1)
